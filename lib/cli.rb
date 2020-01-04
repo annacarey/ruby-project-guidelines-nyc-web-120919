30931ebfc8d56444609e79cffb873898e8631b4b
github username <github email address>
2020-01-04 15:01:59 +0900

    implemented basic login and signup functionality

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,13 +2,36 @@ class CommandLine
     
     PROMPT = TTY::Prompt.new
 
+    @@user = nil
+
     def run 
         welcome
         login
+        #menu
     end 
 
+    # Prints welcome message to the user
     def welcome
-        puts "Welcome to the kindess app!"
+        PROMPT.say("Welcome to the kindess app!")
+    end 
+
+    # Allows user to login or sign up
+    def login
+        choice = PROMPT.select("Would you like to log in or sign up?", ["Login", "Sign up"])
+        if choice == "Login"
+            username = PROMPT.ask("Username:")
+            user = User.find_by(name: username)
+            @@user = user
+        elsif choice == "Sign up"
+            signup
+        end 
+    end
+
+    #Allows the user to sign up for a new account
+    def signup
+        username = PROMPT.ask("Create a username:")
+        password = PROMPT.mask("Create a password:")
+        @@user = User.create(name: username, password: password)
     end 
 
 end 
